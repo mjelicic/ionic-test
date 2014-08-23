44682b998ad0535f9e93e02ea5bc7c7233616876
Miro Jelicic <mjelicic@ebay.com>
2014-08-22 23:51:27 -0400

    refactored model

--- app/scripts/components/models/itemModel.js
+++ app/scripts/components/models/itemModel.js
@@ -5,7 +5,7 @@
 
     .factory('itemModel', function ($firebase) {
 
-      var ref = new Firebase('https://ionic-demo-ng.firebaseio.com/');
+      var ref = new Firebase('https://ionic-demo-ng.firebaseio.com/items');
       var sync = $firebase(ref);
 
       var items = sync.$asArray();
